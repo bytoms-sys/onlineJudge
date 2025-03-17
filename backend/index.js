cf4b5b796bee21191154c1bbf4a2ffe5a840e3d7
bytoms-sys <alokskumar58@gmail.com>
2025-03-18 00:20:56 +0530

    Completed login route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const { DBconnection } = require('./database/db');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('./model/User');
+const cookieParser = require('cookie-parser');
 const app = express();
 
 app.use(express.json());
@@ -20,10 +21,41 @@ app.get('/', (req, res) => {
 app.post('/login', async (req, res) => {
     try {
         //get all the data from the request/frontend/ui/web
+        const { email, password } = req.body;
         //check all the data is present or not which is given by user
+        if(!email || !password) {
+            return res.status(400).json({ error: 'All fields are required', status: false });
+        }
         //find the user with the email in the database
-        //compare the password with the password in the database
-        //store cookie and send the token
+        const emailExist = await User.findOne({
+            email: email
+        });
+        if(emailExist)
+        {
+            if (emailExist.email === email) {
+                //compare the password   with the password in the database
+                const hash = emailExist.password;
+                const isMatch = await bcrypt.compare(password, hash);
+                console.log(isMatch);
+                if(!isMatch) {
+                    return res.status(400).json({json: 'Invalid credes', status: false});
+                }
+                else {
+                    //store cookie and send the token
+                    const token = jwt.sign({ id: emailExist._id, email: emailExist.email }, process.env.JWT_SECRET, { expiresIn: '24h' });
+                    res.cookie("token", token, {
+                        httpOnly: true, // Prevents JavaScript access (XSS protection)
+                        secure: process.env.NODE_ENV === "production", // Only sends over HTTPS in production
+                        sameSite: "Strict", // CSRF protection
+                        maxAge: 24 * 60 * 60 * 1000 // 24 hours expiration
+                    });
+                    return res.status(200).json({ message: 'Login successful', status: true });
+                }
+            }
+        } else {
+            console.log('Email doesn\'t Exist');
+            return res.status(400).json({ error: 'Email doesn\'t exist! Please register', status: false });
+        }
     }
     catch (error) {
         res.send('Error in login route');
